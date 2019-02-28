39505f61d22b405a344772de7e4500e82cd2a769
Noah Ritz <nritz97@gmail.com>
2019-02-27 21:34:11 -0500

    Create a window, renderer and texture, update texture with pixel buffer

--- main.cpp
+++ main.cpp
@@ -5,16 +5,42 @@
 const int WIDTH = 640, HEIGHT = 480;
 
 int main(int argc, char *argv[]) {
-    if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         std::cout << "SDL could not initialize! SDL Error: " << SDL_GetError() << std::endl;
     }
 
-    SDL_Window *window = SDL_CreateWindow( "Hello SDL World", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_ALLOW_HIGHDPI);
+    // Create Window and Renderer
+    SDL_Window *window;
+    SDL_Renderer *renderer;
+    SDL_CreateWindowAndRenderer(WIDTH, HEIGHT, SDL_WINDOW_ALLOW_HIGHDPI, &window, &renderer);
 
-    if (window == NULL) {
-        std::cout << "Could not create window: " << SDL_GetError() << std::endl;
+    if (window == NULL || renderer == NULL) {
+        std::cout << "Could not create window and/or renderer: " << SDL_GetError() << std::endl;
         return EXIT_FAILURE;
     }
+
+    SDL_SetWindowTitle(window, "Game");
+
+    // Create texture to draw pixels to
+    SDL_Texture *texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STREAMING, WIDTH, HEIGHT);
+
+    // Create a pixel buffer
+    Uint32 *pixels = new Uint32[WIDTH*HEIGHT];
+
+    for (int x = 0; x < WIDTH; x++) {
+        for (int y = 0; y < HEIGHT; y++) {
+            if ((x + y) % 10 == 0) {
+                pixels[y*WIDTH + x] = 0xFF2DF5A9;
+            }
+        }
+    }
+
+    SDL_UpdateTexture(texture, NULL, pixels, WIDTH * sizeof(Uint32));
+
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_RenderClear(renderer);
+    SDL_RenderCopy(renderer, texture, NULL, NULL);
+    SDL_RenderPresent(renderer);
 
     SDL_Event windowEvent;
 
